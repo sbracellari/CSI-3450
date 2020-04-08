a9531b85dcc65bf009ac5aa351d1bc859234bead
PastryDisk <43475824+PastryDisk@users.noreply.github.com>
2020-04-08 18:20:51 -0400

    Update main.py

--- Server/main.py
+++ Server/main.py
@@ -1,14 +1,18 @@
-import mysql.connector
+import mysql.connector, json, requests, re
 from mysql.connector import errorcode
+from flask import Flask, jsonify, request
 
+#Database connection
 bankdb = mysql.connector.connect(user='admin', password='SYBBankisawesome1234', host='sybbank.c2ao7w5qbjh5.us-east-2.rds.amazonaws.com', database='banking_db')
 
-mycursor = bankdb.cursor()
+#Flask initialization
+app = Flask(__name__)
 
-mycursor.execute("SELECT * FROM `ACCOUNT`")
+@app.route('/login', methods=['GET'])
+def login():
+    try: 
+        username = request.json.GET('username')
+    else:
+        print("error")
 
-myresult = mycursor.fetchall()
-
-for x in myresult:
-    print(x)
 
